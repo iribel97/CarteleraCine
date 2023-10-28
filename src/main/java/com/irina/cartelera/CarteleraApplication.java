2037acd04f8da978111aacc7e6cf94881eba45d2
iribel97 <irinaochoa32@gmail.com>
2023-10-28 10:01:08 -0500

    Iniciando el proyecto

---
+++ src/main/java/com/irina/cartelera/CarteleraApplication.java
@@ -0,0 +1,13 @@
+package com.irina.cartelera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarteleraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarteleraApplication.class, args);
+	}
+
+}
